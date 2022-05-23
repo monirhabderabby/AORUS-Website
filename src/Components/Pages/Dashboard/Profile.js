eeba80583e3a8ce6713883415ca64fa42e4dbc4a
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-23 12:53:58 +0600

    added text field in profile page

--- src/Components/Pages/Dashboard/Profile.js
+++ src/Components/Pages/Dashboard/Profile.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Profile = () => {
+    const [user] = useAuthState(auth);
     return (
-        <div className="flex justify-center mt-4">
+        <div className="flex mt-4">
             <section className="">
                 <div class="avatar">
                     <div class="w-24 rounded-xl">
@@ -13,9 +16,9 @@ const Profile = () => {
                     <h1 className="text-2xl font-bold text-secondary">Monir Hossain Rabby</h1>
                 </div>
                 <form>
-            <input type="text" value={"Monir Hossain Rabby"} class="input w-full input-bordered max-w-xs" readOnly />
-            <input type="email" value={"monirhrabby.personal@gmail.com"} class="input my-3 w-full input-bordered max-w-xs" readOnly />
-            <input type="text" placeholder="Your age" class="input w-full input-bordered max-w-xs" readOnly />
+            <input type="text" value={user?.displayName} class="input w-full input-bordered max-w-xs" readOnly />
+            <input type="email" value={user?.email} class="input my-3 w-full input-bordered max-w-xs" readOnly />
+            <input type="text" placeholder="Your age" class="input w-full input-bordered max-w-xs"  />
             </form>
             </section>
             
