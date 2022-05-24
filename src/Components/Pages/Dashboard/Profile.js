d3a728ffc9385ad468aab01b4557f90218bfe00a
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-25 00:24:23 +0600

    start for structure of profile page

--- src/Components/Pages/Dashboard/Profile.js
+++ src/Components/Pages/Dashboard/Profile.js
@@ -4,8 +4,18 @@ import auth from "../../../firebase.init";
 
 const Profile = () => {
     const [user] = useAuthState(auth);
+
+    const handleProfile = e => {
+        e.preventDefault();
+        const age = e.target.age.value;
+        const institute = e.target.institute.value;
+        const presentAddress = e.target.presentAddress.value;
+        const parmanentAddress = e.target.parmanentAddress.value;
+        const profile = {age, institute, presentAddress, parmanentAddress}
+        console.log(profile);
+    }
     return (
-        <div className="flex mt-4">
+        <div className="flex justify-center">
             <section className="">
                 <div class="avatar">
                     <div class="w-24 rounded-xl">
@@ -15,10 +25,15 @@ const Profile = () => {
                 <div>
                     <h1 className="text-2xl font-bold text-secondary">Monir Hossain Rabby</h1>
                 </div>
-                <form>
-            <input type="text" value={user?.displayName} class="input w-full input-bordered max-w-xs" readOnly />
-            <input type="email" value={user?.email} class="input my-3 w-full input-bordered max-w-xs" readOnly />
-            <input type="text" placeholder="Your age" class="input w-full input-bordered max-w-xs"  />
+                <form className="flex flex-col w-full" onSubmit={handleProfile}>
+            <input type="text" value={user?.displayName} class="input w-full input-bordered " readOnly />
+            <input type="email" value={user?.email} class="input my-3 w-full input-bordered " readOnly />
+            <input type="date" name="age" placeholder="Your age" class="input w-full mb-3 input-bordered"  />
+            <input type="text" name="institute" placeholder="Your University / College" class="input w-full input-bordered max-w-xs"  />
+            <textarea name="presentAddress" class="textarea textarea-bordered my-3" placeholder="Present Address"></textarea>
+            <textarea name="parmanentAddress" class="textarea textarea-bordered" placeholder="Permanent Address"></textarea>
+                <input type="submit" value="Update" className="btn btn-primary mt-3"/>
+            
             </form>
             </section>
             
