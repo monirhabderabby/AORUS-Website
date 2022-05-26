9e6d19f70a27def4eb9b0f7036fcb844596686e7
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-26 09:54:39 +0600

    added leatest product components

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -4,12 +4,14 @@ import Banner from './Banner';
 import BusinessSummary from './BusinessSummary';
 import HomePageReviews from './HomePageReviews';
 import HomeParts from './HomeParts';
+import LeatestProduct from './LeatestProduct';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <HomeParts />
+            <LeatestProduct />
             <BusinessSummary />
             <HomePageReviews />
             <Footer />
---
+++ src/Components/Pages/Home/LeatestProduct.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import bg from '../../Assets/Images/partsBanner.jpg'
+
+const LeatestProduct = () => {
+    return (
+        <div style={{
+            background: `url(${bg})`
+        }} className="py-3">
+            <div className='flex items-center justify-around'>
+            <div className='text-left text-white'>
+            <h1 className='text-5xl text-white font-bold'>Comming Soon</h1>
+            <p className='my-2'>Very soon we will lounch most powerfull motherboard in the world</p>
+            <button className='btn btn-primary text-white'>SUBSCRIBE</button>
+            </div>
+            <img className='w-[30%]' src="https://static.gigabyte.com/StaticFile/Image/Global/a69f9bb87f60c6790231072d435c9d13/Product/30267/webp/1000" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default LeatestProduct;
